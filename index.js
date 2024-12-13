72608aa4bc469cd1e4f9cc2422f7c968d843e340
altiefermann <163668836+amyhub-alt@users.noreply.github.com>
2024-12-13 09:53:47 -0500

    cors update

--- index.js
+++ index.js
@@ -27,9 +27,27 @@ app.use(bodyParser.urlencoded({  // encodes url automatically
     extended: true
   }));
 
-const cors = require('cors');
-app.use(cors());
+// const cors = require('cors');
+// app.use(cors());
 
+const cors = require('cors');
+let allowedOrigins = [
+  'http://localhost:8080', 
+  'http://testsite.com', 
+  'http://localhost:1234', 
+  'https://dynamic-phoenix-7d9a80.netlify.app'
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 let auth = require('./auth')(app);
 
